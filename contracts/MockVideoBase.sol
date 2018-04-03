9b49a1df4236646b51e58610d8cc1fcd63d38814
BlazeVideo <blazevideoone@gmail.com>
2018-04-03 17:35:09 +0000

    Fix MockVideoBase and use revert

--- contracts/MockVideoBase.sol
+++ contracts/MockVideoBase.sol
@@ -83,7 +83,22 @@ contract MockVideoBase is IVideoBase {
       public
       returns (uint256) {
     // Should not called.
-    require(false);
+    revert();
+  }
+
+
+  /// @dev helper function to transfer the ownership of a video's tokenId.
+  ///   Only accessible to trusted contracts.
+  /// @param _from address which you want to send the token from.
+  /// @param _to address which you want to transfer the token to.
+  /// @param _tokenId uint256 ID of the token of the video to be transferred.
+  function transferVideoTrusted(
+      address _from,
+      address _to,
+      uint256 _tokenId)
+      public {
+    // Should not called.
+    revert();
   }
 
   /// @dev set mock VideoBaseAccessor.
