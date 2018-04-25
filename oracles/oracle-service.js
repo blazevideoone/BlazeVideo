0471c12ebbf1967ebfd3749ba02441715a02493d
BlazeVideo <blazevideoone@gmail.com>
2018-04-25 17:35:26 +0000

    Fix all oracles using truffle exec, fix zepplin-solidty to 1.8.0

--- oracles/oracle-service.js
+++ oracles/oracle-service.js
@@ -1,124 +1,127 @@
-var oracleUtils = require('./utils/oracle-utils.js');
+var oracleUtils = require('./utils/oracle-utils.js')(web3);
 var youtubeAPI = require('./utils/youtube-api.js');
 var config = require('config');
 
 const YOUTUBE_EXAMPLE_ID = "HPPj6viIBmU";
 
-var web3 = oracleUtils.getWeb3();
-
 // Get accounts from web3
-web3.eth.getAccounts((err, accounts) => {
-  var main = async () => {
-    let videoBase = await oracleUtils.getVideoBase();
-    let videoCreator = await oracleUtils.getVideoCreator();
-    let videoAuction = await oracleUtils.getVideoAuction();
-    console.log('VideoBase contract address: ' + videoBase.address);
-    console.log('VideoCreator contract address: ' + videoCreator.address);
-    console.log('VideoAuction contract address: ' + videoAuction.address);
+// no finalCallback
+module.exports = function(finalCallback) {
+  web3.eth.getAccounts((err, accounts) => {
+    var main = async () => {
+      let videoBase = await oracleUtils.getVideoBase();
+      let videoCreator = await oracleUtils.getVideoCreator();
+      let videoAuction = await oracleUtils.getVideoAuction();
+      console.log('VideoBase contract address: ' + videoBase.address);
+      console.log('VideoCreator contract address: ' + videoCreator.address);
+      console.log('VideoAuction contract address: ' + videoAuction.address);
 
-    console.log('Listening to VideoCreator.NewVideoProposed...');
-    videoCreator.NewVideoProposed().watch((err, event) => {
-      if (!!err) {
-        console.log("Found event NewVideoProposed error");
-        console.log(err);
-        console.log(event);
-        return;
-      }
-      var videoId = web3.toAscii(event.args.videoId);
-      console.log("Found event NewVideoProposed videoId: " + videoId);
-
-      var processYouTubeNewVideo = async(youtubeVideoId) => {
-        var TAG = "addNewVideo: ";
-        try {
-          var result = await youtubeAPI.getVideoStatistics(youtubeVideoId);
-          if (result.length <= 0) {
-            console.log(TAG + "YouTube video id " + youtubeVideoId +
-                        " is not found");
-            return;
-          }
-          var stats = result[0].statistics;
-          console.log(TAG + "YouTube video id " + youtubeVideoId +
-                      " viewCount " + stats.viewCount);
-          var contractVideoId = youtubeAPI.YOUTUBE_PREFIX + youtubeVideoId;
-          let estimatedGas = await videoCreator.addNewVideo.estimateGas(
-            web3.fromAscii(contractVideoId),
-            parseInt(stats.viewCount)
-          );
-          await videoCreator.addNewVideo(
-              web3.fromAscii(contractVideoId),
-              parseInt(stats.viewCount),
-              {from: accounts[0], gas: estimatedGas * 2});
-          let tokenId = await videoBase.getTokenId.call(
-              web3.fromAscii(contractVideoId));
-          console.log(TAG + contractVideoId +
-                      " add new tokenId: " + tokenId);
-          console.log(TAG + contractVideoId + " sell price:" +
-              web3.fromWei(await videoAuction.getAuctionPrice(tokenId), "ether")
-              + " ether");
-        } catch (err) {
-          console.log(TAG + "YouTube video id " + youtubeVideoId + " error");
+      console.log('Listening to VideoCreator.NewVideoProposed...');
+      videoCreator.NewVideoProposed().watch((err, event) => {
+        if (!!err) {
+          console.log("Found event NewVideoProposed error");
           console.log(err);
+          console.log(event);
+          return;
         }
-      };
+        var videoId = web3.toAscii(event.args.videoId);
+        console.log("Found event NewVideoProposed videoId: " + videoId);
 
-      if (videoId.startsWith(youtubeAPI.YOUTUBE_PREFIX)) {
-        var youtubeVideoId = videoId.substring(youtubeAPI.YOUTUBE_PREFIX.length,
-                                               youtubeAPI.YOUTUBE_PREFIX.length +
-                                               YOUTUBE_EXAMPLE_ID.length);
-        processYouTubeNewVideo(youtubeVideoId);
-      }
-    });
-    // End of VideoCreator.NewVideoProposed
-
-    console.log('Listening to VideoCreator.VideoUpdateRequested...');
-    videoCreator.VideoUpdateRequested().watch((err, event) => {
-      if (!!err) {
-        console.log("Found event VideoUpdateRequested error");
-        console.log(err);
-        console.log(event);
-        return;
-      }
-      var videoId = web3.toAscii(event.args.videoId);
-      console.log("Found event VideoUpdateRequested videoId: " + videoId);
-
-      var updateYouTubeVideo = async(youtubeVideoId) => {
-        var TAG = "updateVideo: ";
-        try {
-          var result = await youtubeAPI.getVideoStatistics(youtubeVideoId);
-          if (result.length <= 0) {
+        var processYouTubeNewVideo = async(youtubeVideoId) => {
+          var TAG = "addNewVideo: ";
+          try {
+            var result = await youtubeAPI.getVideoStatistics(youtubeVideoId);
+            if (result.length <= 0) {
+              console.log(TAG + "YouTube video id " + youtubeVideoId +
+                          " is not found");
+              return;
+            }
+            var stats = result[0].statistics;
             console.log(TAG + "YouTube video id " + youtubeVideoId +
-                        " is not found");
-            return;
+                        " viewCount " + stats.viewCount);
+            var contractVideoId = youtubeAPI.YOUTUBE_PREFIX + youtubeVideoId;
+            // Disable estimateGas
+            // let estimatedGas = await videoCreator.addNewVideo.estimateGas(
+            //   web3.fromAscii(contractVideoId),
+            //   parseInt(stats.viewCount)
+            // );
+            await videoCreator.addNewVideo(
+                web3.fromAscii(contractVideoId),
+                parseInt(stats.viewCount),
+                {from: accounts[0] /*, gas: estimatedGas * 2 */});
+            let tokenId = await videoBase.getTokenId.call(
+                web3.fromAscii(contractVideoId));
+            console.log(TAG + contractVideoId +
+                        " add new tokenId: " + tokenId);
+            console.log(TAG + contractVideoId + " sell price:" +
+                web3.fromWei(await videoAuction.getAuctionPrice(tokenId), "ether")
+                + " ether");
+          } catch (err) {
+            console.log(TAG + "YouTube video id " + youtubeVideoId + " error");
+            console.log(err);
           }
-          var stats = result[0].statistics;
-          console.log(TAG + "YouTube video id " + youtubeVideoId +
-                      " viewCount " + stats.viewCount);
-          var contractVideoId = youtubeAPI.YOUTUBE_PREFIX + youtubeVideoId;
-          await videoCreator.updateVideo(
-              web3.fromAscii(contractVideoId),
-              parseInt(stats.viewCount),
-              {from: accounts[0]});
-          let tokenId = await videoBase.getTokenId.call(
-              web3.fromAscii(contractVideoId));
-          console.log(TAG + contractVideoId +
-                      " update tokenId: " + tokenId);
-          console.log(TAG + contractVideoId + " view count:" +
-              await videoBase.getVideoViewCount(contractVideoId));
-        } catch (err) {
-          console.log(TAG + "YouTube video id " + youtubeVideoId + " error");
-          console.log(err);
+        };
+
+        if (videoId.startsWith(youtubeAPI.YOUTUBE_PREFIX)) {
+          var youtubeVideoId = videoId.substring(youtubeAPI.YOUTUBE_PREFIX.length,
+                                                 youtubeAPI.YOUTUBE_PREFIX.length +
+                                                 YOUTUBE_EXAMPLE_ID.length);
+          processYouTubeNewVideo(youtubeVideoId);
         }
-      };
+      });
+      // End of VideoCreator.NewVideoProposed
 
-      if (videoId.startsWith(youtubeAPI.YOUTUBE_PREFIX)) {
-        var youtubeVideoId = videoId.substring(youtubeAPI.YOUTUBE_PREFIX.length,
-                                               youtubeAPI.YOUTUBE_PREFIX.length +
-                                               YOUTUBE_EXAMPLE_ID.length);
-        updateYouTubeVideo(youtubeVideoId);
-      }
-    });
-    // End of VideoCreator.VideoUpdateRequested
-  };
+      console.log('Listening to VideoCreator.VideoUpdateRequested...');
+      videoCreator.VideoUpdateRequested().watch((err, event) => {
+        if (!!err) {
+          console.log("Found event VideoUpdateRequested error");
+          console.log(err);
+          console.log(event);
+          return;
+        }
+        var videoId = web3.toAscii(event.args.videoId);
+        console.log("Found event VideoUpdateRequested videoId: " + videoId);
 
-  main();
-})
+        var updateYouTubeVideo = async(youtubeVideoId) => {
+          var TAG = "updateVideo: ";
+          try {
+            var result = await youtubeAPI.getVideoStatistics(youtubeVideoId);
+            if (result.length <= 0) {
+              console.log(TAG + "YouTube video id " + youtubeVideoId +
+                          " is not found");
+              return;
+            }
+            var stats = result[0].statistics;
+            console.log(TAG + "YouTube video id " + youtubeVideoId +
+                        " viewCount " + stats.viewCount);
+            var contractVideoId = youtubeAPI.YOUTUBE_PREFIX + youtubeVideoId;
+            await videoCreator.updateVideo(
+                web3.fromAscii(contractVideoId),
+                parseInt(stats.viewCount),
+                {from: accounts[0]});
+            let tokenId = await videoBase.getTokenId.call(
+                web3.fromAscii(contractVideoId));
+            console.log(TAG + contractVideoId +
+                        " update tokenId: " + tokenId);
+            let videoInfo = await videoBase.getVideoInfo(tokenId);
+            console.log(TAG + contractVideoId + " view count:" +
+                videoInfo[2]);
+          } catch (err) {
+            console.log(TAG + "YouTube video id " + youtubeVideoId + " error");
+            console.log(err);
+          }
+        };
+
+        if (videoId.startsWith(youtubeAPI.YOUTUBE_PREFIX)) {
+          var youtubeVideoId = videoId.substring(youtubeAPI.YOUTUBE_PREFIX.length,
+                                                 youtubeAPI.YOUTUBE_PREFIX.length +
+                                                 YOUTUBE_EXAMPLE_ID.length);
+          updateYouTubeVideo(youtubeVideoId);
+        }
+      });
+      // End of VideoCreator.VideoUpdateRequested
+    };
+
+    main();
+  })
+};
--- oracles/request-video-update.js
+++ oracles/request-video-update.js
@@ -1,34 +1,43 @@
-var oracleUtils = require('./utils/oracle-utils.js');
+var oracleUtils = require('./utils/oracle-utils.js')(web3);
 var youtubeAPI = require('./utils/youtube-api.js');
-var Web3 = require('web3');
 var path = require('path');
 var config = require('config');
+var myArgs = require('optimist').argv;
 
-if (process.argv.length <= 3) {
-  console.log("ERROR missing argument...");
-  console.log("Usage: node " + path.basename(__filename) + " YoutubeVideoId ether");
-  console.log("Parameter: YoutubeVideoId the newly proposed Youtube video id," +
-              " without prefix.");
-  console.log("Parameter: ether cost in ether for update.");
-  return;
-}
 
-var youtubeVideoId = youtubeAPI.YOUTUBE_PREFIX +
-                     process.argv[process.argv.length - 2];
-var web3 = oracleUtils.getWeb3();
-var cost = web3.toWei(parseFloat(process.argv[process.argv.length - 1]), 'ether');
+module.exports = function(finalCallback) {
 
-// Get accounts from web3
-web3.eth.getAccounts((err, accounts) => {
-  var main = async () => {
-    let videoBase = await oracleUtils.getVideoBase();
-    let videoCreator = await oracleUtils.getVideoCreator();
-    console.log('VideoCreator contract address: ' + videoCreator.address);
-    console.log("Request video update: " + youtubeVideoId);
-    let tokenId = await videoBase.getTokenId(web3.fromAscii(youtubeVideoId));
-    await videoCreator.requestVideoUpdate(
-        tokenId,
-        {from: accounts[0], value: cost});
-  };
-  main();
-})
+  if (!myArgs.video || !myArgs.ether) {
+    console.log("ERROR missing argument...");
+    console.log("Usage: truffle --network ... exec " +
+                path.basename(__filename) +
+                " --video YoutubeVideoId --ether ether");
+    console.log("Parameter: YoutubeVideoId the newly proposed Youtube video id," +
+                " without prefix.");
+                console.log("Parameter: YoutubeVideoId the newly proposed Youtube video id," +
+                            " without prefix.");
+                console.log("Parameter: ether cost in ether for update.");
+    return;
+  }
+
+  var youtubeVideoId = youtubeAPI.YOUTUBE_PREFIX + myArgs.video;
+  var cost = web3.toWei(parseFloat(myArgs.ether), 'ether');
+
+  // Get accounts from web3
+  web3.eth.getAccounts((err, accounts) => {
+    var main = async () => {
+      let videoBase = await oracleUtils.getVideoBase();
+      let videoCreator = await oracleUtils.getVideoCreator();
+      console.log('VideoCreator contract address: ' + videoCreator.address);
+      let tokenId = await videoBase.getTokenId(web3.fromAscii(youtubeVideoId));
+      console.log("Request video update: " + youtubeVideoId +
+                  " tokenId: " + tokenId);
+      await videoCreator.requestVideoUpdate(
+          tokenId,
+          {from: accounts[0], value: cost});
+      finalCallback();
+    };
+    main();
+  })
+
+};
