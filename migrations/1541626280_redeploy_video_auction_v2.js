2f487e54b40375cc07cf71e90f330248b1c6031c
vanship <22755027@qq.com>
2018-05-06 17:16:11 +0000

    Add redeploy scripts

---
+++ migrations/1541626280_redeploy_video_auction_v2.js
@@ -0,0 +1,52 @@
+var VideoBase = artifacts.require("./VideoBase.sol");
+var VideoAuction = artifacts.require("./VideoAuction.sol");
+var sleep = require("../oracles/utils/sleep.js");
+
+module.exports = function(deployer) {
+  let videoBase;
+  let videoAuction;
+  deployer.queueOrExec(function() {
+    return sleep();
+  }).then(function() {
+    return VideoBase.deployed();
+  }).then(function(instance) {
+    videoBase = instance;
+    return VideoAuction.deployed();
+  }).then(function(instance) {
+    videoAuction = instance;
+    return videoAuction.resetVideoBase();
+  }).then(function() {
+    return sleep();
+  }).then(function() {
+    return videoBase.removeTrustedContract(videoAuction.address);
+  }).then(function() {
+    return sleep();
+  }).then(function() {
+    return videoBase.removeListener(videoAuction.address);
+  }).then(function() {
+    return sleep();
+  }).then(function() {
+    return deployer.deploy(VideoAuction);
+  }).then(function() {
+    return sleep();
+  }).then(function() {
+    return VideoBase.deployed();
+  }).then(function(instance) {
+    videoBase = instance;
+    return VideoAuction.deployed();
+  }).then(function(instance) {
+    videoAuction = instance;
+    return videoAuction.setVideoBase(videoBase.address);
+  }).then(function() {
+    return sleep();
+  }).then(function() {
+    return videoBase.addTrustedContract(videoAuction.address);
+  }).then(function() {
+    return sleep();
+  }).then(function() {
+    return videoBase.addListener(videoAuction.address);
+  }).then(function() {
+    return sleep();
+  }).then(function() {
+  });
+};
