fb9f8cb18579db0cc0de1d5304682f4f66ff7636
SPC <ps1381@nyu.edu>
2018-05-09 15:50:42 +0800

    add metamask icon and loading page

--- src/layouts/accountlocked/AccountLocked.js
+++ src/layouts/accountlocked/AccountLocked.js
@@ -6,8 +6,13 @@ export default class AccountLocked extends Component {
     return(
       <Container>
         <Row>
+          <Col className="text-center">
+            <a href="https://metamask.io" target="blank"><img src="img/download-metamask.png" width="500px" style={{margin: '50px auto'}}alt="Download Metamask"/></a>
+          </Col>
+        </Row>
+        <Row>
           <Col>
-            <h3 style={{textAlign: 'center'}}>The MetaMask is not installed or locked. Please install or unlock MetaMask and go Back.</h3>
+            <p style={{textAlign: 'center'}}>The MetaMask is not installed or locked. <br />Please install or unlock MetaMask and go Back.</p>
           </Col>
         </Row>
       </Container>
